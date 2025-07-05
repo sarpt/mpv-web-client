58bd10d72fe3f84937e9407315e24ae11444ae34
sarpt <mike.m.info@gmail.com>
2025-07-05 09:20:31 +0200

    move semver to a separate module

--- src/frontend/pkg_manifest.rs
+++ src/frontend/pkg_manifest.rs
@@ -1,12 +1,14 @@
-use serde::{Deserialize, Deserializer};
+use serde::Deserialize;
 use std::{fs::rename, io::Read, path::Path};
 
 use crate::{
-  frontend::FrontendPkgErr,
+  frontend::{FrontendPkgErr, pkg_manifest::semver::Semver},
   project_paths::{get_frontend_dir, get_frontend_temp_dir, get_project_home_dir},
 };
 
 pub const PKG_MANIFEST_NAME: &str = "pkg_manifest.toml";
+
+pub mod semver;
 
 #[derive(Deserialize, PartialEq)]
 pub struct VersionInfo {
@@ -66,56 +68,4 @@ pub fn move_manifest_to_project_home() -> Result<(), FrontendPkgErr> {
     path
   };
   rename(manifest_file_path, new_manifest_file_path).map_err(FrontendPkgErr::HomeDirInaccessible)
-}
-
-#[derive(PartialEq, PartialOrd)]
-pub struct Semver {
-  major: usize,
-  minor: usize,
-  patch: usize,
-}
-
-const VERSION_SEMVER_SEPARATOR: &str = ".";
-impl<'de> Deserialize<'de> for Semver {
-  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-  where
-    D: Deserializer<'de>,
-  {
-    let version = String::deserialize(deserializer)?;
-    Semver::from_string(&version).map_err(serde::de::Error::custom)
-  }
-}
-
-impl Semver {
-  fn from_string(source: &String) -> Result<Self, String> {
-    let mut split_version = source.split(VERSION_SEMVER_SEPARATOR).map(|chunk| {
-      chunk
-        .parse::<usize>()
-        .map_err(|err| format!("could not parse source string of \"{source}\" as semver: {err}"))
-    });
-    let major: usize = split_version.nth(0).unwrap_or(Ok(0))?;
-    let minor: usize = split_version.nth(1).unwrap_or(Ok(0))?;
-    let patch: usize = split_version.nth(2).unwrap_or(Ok(0))?;
-    Ok(Semver {
-      major,
-      minor,
-      patch,
-    })
-  }
-}
-
-impl TryFrom<&String> for Semver {
-  type Error = String;
-
-  fn try_from(value: &String) -> Result<Self, Self::Error> {
-    Semver::from_string(value)
-  }
-}
-
-impl From<Semver> for String {
-  fn from(val: Semver) -> Self {
-    [val.major, val.minor, val.patch]
-      .map(|chunk| chunk.to_string())
-      .join(VERSION_SEMVER_SEPARATOR)
-  }
 }
