4e6dc13da15e3cca971600cfa9a35e37b97de6be
sarpt <mike.m.info@gmail.com>
2025-07-05 14:23:24 +0200

    Add options to update and force outdated pkg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
-use log::{debug, error};
+use log::{debug, error, info};
 use std::{error::Error, path::PathBuf, time::SystemTime};
 
 use crate::{
   frontend::{
-    check_frontend_pkg, newer_remote_release_available,
-    releases::fetch_remote_frontend_package_release,
+    RemoteReleaseCheckResult, check_for_newer_remote_release, check_frontend_pkg,
+    releases::{Release, fetch_remote_frontend_package_release},
   },
   project_paths::ensure_project_dirs,
   server::serve,
@@ -19,12 +19,29 @@ mod server;
 #[command(version = "0.1.0", about = "client for mpv-web-api and mpv-web-front server", long_about = None)]
 struct Args {
   #[arg(
-    short,
     long,
     required = false,
-    help = "Path to a .tar.gz frontend package"
+    help = "Path to a .tar.gz frontend package. Overrides --update."
   )]
   pkg: Option<PathBuf>,
+
+  #[arg(
+    action,
+    short = 'u',
+    long,
+    required = false,
+    help = "Update to newer frontend package, if any exists on remote repository. Does not apply when --pkg provided."
+  )]
+  update: bool,
+
+  #[arg(
+    action,
+    short = 'f',
+    long,
+    required = false,
+    help = "Force installation of provided outdated frontend package with --pkg, even if the newer package is already being served."
+  )]
+  force_outdated: bool,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -35,35 +52,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
   debug!("version {}", env!("CARGO_PKG_VERSION"));
 
   ensure_project_dirs()?;
-  init_frontend(&args.pkg)
+  init_frontend(&args)
     .await
     .map_err(|err_msg| *Box::new(err_msg))?;
   serve().await
 }
 
-async fn init_frontend(pkg_path: &Option<PathBuf>) -> Result<(), String> {
-  let mut path_pkg = pkg_path.to_owned();
+async fn init_frontend(args: &Args) -> Result<(), String> {
+  let mut pkg_path = args.pkg.to_owned();
 
-  if path_pkg.is_none() {
-    match newer_remote_release_available().await {
-      Ok(result) => {
-        if let Some(new_release) = result {
-          path_pkg = match fetch_remote_frontend_package_release(&new_release).await {
-            Ok(path_pkg) => Some(path_pkg),
-            Err(err) => {
-              error!("fetch of remote frontend package failed: {err}");
-              None
-            }
-          }
-        }
-      }
-      Err(err) => {
-        error!("check for the latest remote package failed: {err}");
-      }
-    };
+  if pkg_path.is_none() {
+    if let Some(new_release) = remote_frontend_release_available(args.update).await {
+      info!(
+        "fetching new frontend package version \"{}\"",
+        new_release.name
+      );
+      pkg_path = fetch_new_frontend_release(&new_release).await;
+    }
   }
 
-  let result = tokio::task::spawn_blocking(|| check_frontend_pkg(path_pkg)).await;
+  let result = tokio::task::spawn_blocking(|| check_frontend_pkg(pkg_path)).await; // TODO: this should be public async fn frontend::install_package
   let check_frontend_result = match result {
     Ok(res) => res,
     Err(e) => return Err(format!("issue with joining on blocking task {e}")),
@@ -72,6 +80,43 @@ async fn init_frontend(pkg_path: &Option<PathBuf>) -> Result<(), String> {
   match check_frontend_result {
     Ok(_) => Ok(()),
     Err(err) => Err(format!("frontend init failed: {err}")),
+  }
+}
+
+async fn remote_frontend_release_available(allow_updates: bool) -> Option<Release> {
+  match check_for_newer_remote_release().await {
+    Ok(result) => match result {
+      RemoteReleaseCheckResult::UpToDate(local) => {
+        info!("local frontend version \"{local}\" is up to date");
+        None
+      }
+      RemoteReleaseCheckResult::NewerRemoteAvailable(new_release) => {
+        if allow_updates {
+          Some(new_release)
+        } else {
+          info!(
+            "newer frontend release \"{}\" is available - run the program with \"--update\" argument to install it",
+            new_release.name
+          );
+          None
+        }
+      }
+      RemoteReleaseCheckResult::RemoteNecessary(release) => Some(release),
+    },
+    Err(err) => {
+      error!("check for the latest remote package failed: {err}");
+      None
+    }
+  }
+}
+
+async fn fetch_new_frontend_release(new_release: &Release) -> Option<PathBuf> {
+  match fetch_remote_frontend_package_release(new_release).await {
+    Ok(path_pkg) => Some(path_pkg),
+    Err(err) => {
+      error!("fetch of remote frontend package failed: {err}");
+      None
+    }
   }
 }
 
